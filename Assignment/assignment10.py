80ba9ac83a8b83f636a221260252b545165e77ec
Titli Basu <titlibasu6790@gmail.com>
2025-09-12 23:53:47 +0530

    assignment10_12/09/25

---
+++ Assignment/assignment10.py
@@ -0,0 +1,20 @@
+'''
+Assignment 3: 
+Validate Phone Numbers with re.match.
+Given a list of phone numbers, print only the ones that match this format: (123) 456-7890
+'''
+
+import re
+
+phone_numbers = [
+    "(123) 456-7890",
+    "123-456-7890",
+    "(987) 654-3210",
+    "(123 456-7890)"
+]
+
+pattern = r"^\(\d{3}\) \d{3}-\d{4}$"
+
+for number in phone_numbers:
+    if re.match(pattern, number):
+        print(number)
